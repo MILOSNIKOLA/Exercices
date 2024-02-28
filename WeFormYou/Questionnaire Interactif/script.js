5352ae1b7d8693e411818f180175c83137bc101f
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2024-02-28 18:23:12 +0100

    Add files via upload

---
+++ WeFormYou/Questionnaire Interactif/script.js
@@ -0,0 +1,22 @@
+const question = [
+  {
+    question:
+      'A quel compositeur est consacré le film Amadeus ? \n1. Beethoven \n2. Mozart',
+    reponse: 2
+  },
+  {
+    question: "Quelle est la capitale de l'Autriche ? \n1. Vienne \n2. Oslo",
+    reponse: 1
+  },
+  {
+    question:
+      'Quel organe est perturbé par les acouphènes ? \n1. Les Pieds \n2. Les Oreilles',
+    reponse: 2
+  }
+];
+ 
+for (let i = 0; i < question.length; i++) {
+  console.log(i);
+  
+}
+
