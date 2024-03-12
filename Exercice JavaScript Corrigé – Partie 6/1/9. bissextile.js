cccd78946a0e4c49d930ff4fa35b5895947a16e2
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2024-03-12 19:35:18 +0100

    Add files via upload

---
+++ Exercice JavaScript Corrigé – Partie 6/1/9. bissextile.js
@@ -0,0 +1,35 @@
+// Exercice 9:
+
+// Une année bissextile a un jour ajouté à février pour être synchronisée avec l’année saisonnière. Une année bissextile apparaît avec une fréquence régulière, déterminée par la règle ci-dessous:
+
+// Une année doit être divisible par 400 ou divisible par 4 et non pas 100.
+
+// Vous devez implémenter une fonction qui renvoie TRUE si c’est une année bissextile, ou FALSE si ce n’est pas le cas.
+
+// Exemple:
+
+// bissextile(2020) ➞ true
+// // Divisé par 4 et non par 100.
+
+// bissextile(1400) ➞ false
+// // Divisé par 4, mais également divisé par 100.
+
+// bissextile(2000) ➞ true
+// // Divisé par 400.
+
+function bissextile(year) {
+
+	// Écrivez votre code ici
+
+    
+return 
+
+}
+
+
+
+// Afficher la sortie
+
+console.log(bissextile(2020));
+console.log(bissextile(1400));
+console.log(bissextile(2000));
