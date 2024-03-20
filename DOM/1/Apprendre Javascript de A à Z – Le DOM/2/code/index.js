65e8a9ff3d81a103989cefdf95d233582128dc0f
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2024-03-20 16:38:56 +0100

    Add files via upload

--- DOM/1/Apprendre Javascript de A à Z – Le DOM/2/code/index.js
+++ DOM/1/Apprendre Javascript de A à Z – Le DOM/2/code/index.js
@@ -23,29 +23,44 @@ const response = document.querySelector('p');
 // questionContainer.style.borderRadius = '150px';
 
 questionContainer.addEventListener('click' , () => {
-    //console.log('okeee'); // Verifier TOUJOUR AVANT
+    console.log('okeee'); // Verifier TOUJOUR AVANT
     // questionContainer.style.background = "red"
     questionContainer.classList.toggle('question-clicked')
-    questionContainer.style.borderRadius = '150px';
+    
 })
 
 btn1.addEventListener('click', () => {
     // console.log('clickkkkk');
     response.style.visibility = "visible";
-    response.style.background = "green"
+    response.style.background = "green";
+    questionContainer.style.borderRadius = '150px';
 })
 
-// btn2.addEventListener('click', () => {
-//     // console.log('clickkkkk');
-//     response.style.visibility = "visible";
-//     response.style.color = 'white';
-//     // response.classList.toggle('wrong')
-//     questionContainer.style.textDecorationLine = 'normal';
-//     response.style.background = 'red';
-//     questionContainer.classList.toggle('question-clicked1')
-// })
+btn2.addEventListener('click', () => {
+    // console.log('clickkkkk');
+    response.style.visibility = "visible";
+    response.style.color = 'white';
+    // response.classList.toggle('wrong')
+    // questionContainer.style.textDecorationLine = 'normal';
+    response.style.background = 'red';
+    questionContainer.classList.toggle('question-clicked1');
+    questionContainer.style.borderRadius = '150px';
+})
 
  
+// /v3/polyfill.js
 
+// /v3/polyfill.min.js.
 
-
+Uncaught (in promise) Error: Internal error opening backing store for indexedDB.open.
+    at wrappedSendMessageCallback (browser-polyfill.js:1163:16)
+wrappedSendMessageCallback @ browser-polyfill.js:1163
+await in wrappedSendMessageCallback (asynchrone)
+(anonyme) @ index.js:92
+(anonyme) @ index.js:92
+(anonyme) @ index.js:92
+polyfill.js:1193 Uncaught (in promise) Error: Internal error opening backing store for indexedDB.open.
+    at wrappedSendMessageCallback (polyfill.js:1193:18)
+wrappedSendMessageCallback @ polyfill.js:1193
+browser-polyfill.js:1163 Uncaught (in promise) Error: Internal error opening backing store for indexedDB.open.
+    at wrappedSendMessageCallback (browser-polyfill.js:1163:16)
