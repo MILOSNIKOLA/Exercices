efb7060de417bdbce468c5d7031a4611c5dcd86a
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2024-04-03 19:17:34 +0200

    Add files via upload

--- DOM/1/Apprendre Javascript de A à Z – Le DOM/2/code/index.js
+++ DOM/1/Apprendre Javascript de A à Z – Le DOM/2/code/index.js
@@ -303,4 +303,9 @@ window.onload = () => {
     // location.href = 'https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile', 5000
 };
 
+/********************************* SetProperty ***************************************/
 
+window.addEventListener('mousemove', (e) => {
+    nav.style.setProperty('--x', e.layerX + 'px')
+    nav.style.setProperty('--y', e.layerY + 'px')
+})
