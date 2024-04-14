3f574882ea4ed2e5c04e331209515b6952da3b1e
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2024-04-14 14:56:43 +0200

    Add files via upload

---
+++ Waytolearnx JS/Exercice JavaScript Corrigé – Partie 7/1. nbrPaire.js
@@ -0,0 +1,25 @@
+// Exercice 1:
+
+// Créez une fonction qui trouve tous les nombres pairs de 1 au nombre donné.
+
+// Exemple:
+
+// nbrPaire(6) ➞ [2, 4, 6]
+
+// nbrPaire(4) ➞ [2, 4]
+
+// nbrPaire(2) ➞ [2]  
+    
+function nbrPaire(n) {
+
+	// Écrivez votre code ici
+
+    
+
+}
+
+// Afficher la sortie
+
+console.log(nbrPaire(6));
+console.log(nbrPaire(4));
+console.log(nbrPaire(2));
