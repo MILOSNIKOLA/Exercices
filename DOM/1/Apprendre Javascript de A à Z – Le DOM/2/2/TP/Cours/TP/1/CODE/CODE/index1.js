73590cd6c4d3b784822b393d07295f21996efd24
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2024-06-25 18:28:19 +0200

    Add files via upload

---
+++ DOM/1/Apprendre Javascript de A à Z – Le DOM/2/2/TP/Cours/TP/1/CODE/CODE/index1.js
@@ -0,0 +1,35 @@
+// SELECTEURS
+
+// document.querySelector('h4').style.background = "red" ako je jedan
+
+// const baliseHTML = document.querySelector('h4')      - declarer
+
+// console.log(baliseHTML);                             - utiliser
+
+
+//  Click event 
+
+const questionContainer = document.querySelector('.click-event')
+
+questionContainer.addEventListener('click', () => {
+    questionContainer.style.background = 'red'
+    questionContainer.style.border = '3px solid grey'
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
