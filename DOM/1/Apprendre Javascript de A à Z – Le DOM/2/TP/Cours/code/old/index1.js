49457bcd13859ef264d5ffe02869273f166f0048
MILOSNIKOLA <43822077+MILOSNIKOLA@users.noreply.github.com>
2024-06-25 11:17:11 +0200

    Add files via upload

---
+++ DOM/1/Apprendre Javascript de A à Z – Le DOM/2/TP/Cours/code/old/index1.js
@@ -0,0 +1,34 @@
+
+// SELECTEURS
+// document.querySelector('h4').style.background = "blue";
+
+// const baliseHTML = document.querySelector('h4');
+
+//  console.log(baliseHTML);
+
+//--------------------------------  Click event  -----------------------------
+
+//const questionContainer = document.querySelector(".click-event");
+
+// console.log('questionContainer');
+
+// questionContainer.style.borderRadius = '150px';
+
+// questionContainer.addEventListener("click", () => {
+//     // console.log("Click !!!!");
+//     questionContainer.style.background = "red";
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
